b532605347f42c4c8a2593c5ebf118375f018908
topera <topera@gmail.com>
2018-02-10 12:37:37 -0200

    Adding tree test

---
+++ src/test/groovy/com/topera/angel/tree/MainSpec.groovy
@@ -0,0 +1,16 @@
+package com.topera.angel.tree
+
+
+import spock.lang.Specification
+
+/**
+ * Created by topera on 23/07/17.
+ */
+class MainSpec extends Specification {
+
+    def "test"() {
+        expect:
+        Main.main()
+    }
+
+}
